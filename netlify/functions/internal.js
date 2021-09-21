c69e6ecf753b17fd2e50997496b2eaedc12628fd
alann-maulana <alann_it06@yahoo.co.id>
2021-09-21 13:21:27 +0700

    Create internal.js

---
+++ netlify/functions/internal.js
@@ -0,0 +1,25 @@
+const fetch = require("node-fetch");
+
+exports.handler = async (event, context) => {
+    try {
+        var response = await fetch('/.netlify/functions/test?username=password');
+        const data = await response.json();
+        return {
+            statusCode: 200,
+            headers: {
+                'Content-type': 'application/json'
+            },
+            body: JSON.stringify(data),
+        };
+    } catch (error) {
+        return {
+            statusCode: 422,
+            headers: {
+                'Content-type': 'application/json'
+            },
+            body: JSON.stringify({
+                'error': String(error)
+            })
+        };
+    }
+};
